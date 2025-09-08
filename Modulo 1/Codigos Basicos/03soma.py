a69b157b3f64e00f8e39744f38ee15c6ac2e6000
Joao-VMD <joaovictormogliadantas@gmail.com>
2025-09-08 09:39:06 -0300

    Add files via upload

---
+++ Modulo 1/Codigos Basicos/03soma.py
@@ -0,0 +1,22 @@
+print("Digite o valor 1 : " )
+valor1 = int(input()) 
+print("Digite o valor 2 : " )
+valor2 = int(input())
+
+
+soma = valor1 + valor2
+
+print(f"A soma dos valores é : {soma}")
+
+
+print("Digite o valor 1 : ") 
+valor1 = float(input())
+print("Digite o valor 2 : ")
+valor2 = float(input())
+
+soma1 = valor1 + valor2
+
+print(f"A soma dos valores é : {soma1}")
+
+resultado = soma + soma1
+print(f"a soma total é : {resultado}")
