c1caa49b11249be662a3c8a0bac005c8c66245c2
Joao-VMD <joaovictormogliadantas@gmail.com>
2025-09-08 10:12:15 -0300

    Add files via upload

---
+++ Modulo 1/Loopings/variaveis.py
@@ -0,0 +1,10 @@
+n1 = int(input("digite o numero: "))
+def multiplicaçao(n1, n2):
+    return n1 * n2
+
+for i in range(1,11):
+   print(multiplicaçao(4,i))
+
+
+
+ 
---
+++ Modulo 1/Loopings/while.py
@@ -0,0 +1,6 @@
+numero = int(input("digite o numero da tabuada: "))
+i = 1
+while i <= 10 :
+    print(i*numero)
+    i = i + 1
+    
