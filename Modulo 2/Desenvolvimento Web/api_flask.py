38e04be2943650299a0e0e01ab4da66caf28b9b2
Joao-VMD <joaovictormogliadantas@gmail.com>
2025-09-10 11:14:46 -0300

    Add files via upload

---
+++ Modulo 2/Desenvolvimento Web/api_flask.py
@@ -0,0 +1,37 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+playlist = [
+    {"id":1, "titulo":"Always On Time", "artista":"Ja Rule", "link":"https://youtu.be/0tcDXJfAFVw?si=MI9fxqbS1o94CD-V"},
+    {"id":2, "titulo":" Mesmerize", "artista":"Ja Rule", "link":"https://youtu.be/VcP96KbFIIU?si=wqy9iIRYhHHiuNqB"},
+    {"id":3, "titulo":"baile inolvidable", "artista":"Bad Bunny", "link":"https://youtu.be/a1Femq4NPxs?si=Hs8e7oxuMxHwBkdC"}
+    ]
+
+@app.route("/musicas", methods=['GET'])
+def get_musicas():
+    return jsonify({"playlist":playlist, "total":len(playlist)})
+
+@app.route("/musicas/<int:id>", methods=["GET"])
+def get_musica(id):
+    for musica in playlist:
+        if musica["id"] == id:
+            return jsonify(musica), 200
+    return "",204    
+
+
+@app.route("/musicas", methods=['POST'])
+def add_musica():
+    if request.is_json:
+        nova_musica = request.get_json()
+    else:
+        nova_musica = request.form.to_dict()
+
+    nova_musica["id"] = len(playlist) + 1
+
+    playlist.append(nova_musica)
+    return jsonify({"mensagem": "Nova música adicionada!", "musica":nova_musica}), 201
+
+app.run(host="0.0.0.0", debug=True)
---
+++ Modulo 2/Desenvolvimento Web/app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+
+st.image("https://img.stablecog.com/insecure/1536w/aHR0cHM6Ly9iLnN0YWJsZWNvZy5jb20vYzVlZDE5ZjQtMGU4ZS00MDQ4LWEzMGMtMWY3ZjQzYmM0OWMzLmpwZWc.webp")
+st.title("wolffy - Suas Músicas Favoritas Aqui! 🎶😎")
+st.caption("Criado com Streamlit ❤️")
+st.link_button("Adicionar musica",url="http://127.0.0.1:5500/Extra%20html/index.html")
